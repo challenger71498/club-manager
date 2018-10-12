217bafcf564cc6961f7c38ee7d4925010cfa36cc
Clestar <clestar73@gmail.com>
2018-10-12 20:09:32 +0900

    mysql 비밀번호 변경

--- backend/libs/mysql.js
+++ backend/libs/mysql.js
@@ -2,7 +2,7 @@ const mysql = require('promise-mysql');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '1234',
+    password: '12345678',
     database: 'club_manager'
 });
 
