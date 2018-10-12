dd40108185f98e002f68cb3cdcc86b473c09f2b7
skd6150 <skd6150@gmail.com>
2018-10-12 19:28:15 +0900

    Fix backend route bug

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,10 @@ require('express-async-errors');
 
 const app = express();
 
-app.use(require('connect-history-api-fallback')());
-app.use(express.static(path.resolve(__dirname, 'static')));
-
 app.use('/api', require('./routes/index'));
+
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(require('connect-history-api-fallback')());
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
