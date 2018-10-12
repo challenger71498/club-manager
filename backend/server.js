0c9eae89e1ef2f92c408ed4382be7a6aac65c649
Clestar <clestar73@gmail.com>
2018-10-12 19:40:17 +0900

    Fix reload page error

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,10 @@ app.use('/api', require('./routes/index'));
 app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(require('connect-history-api-fallback')());
 
+app.use((req, res, next) => {
+    res.sendFile(path.resolve(__dirname, 'static/index.html')) ;
+});
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
