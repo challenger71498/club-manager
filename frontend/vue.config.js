7378f5110c55bbf30e9c4d4def8cb6569ef6e2e9
Jo Tae Sang <gsts007@gmail.com>
2018-10-13 08:47:23 +0900

    Add alias

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,16 @@
+const path = require('path');
+
 module.exports = {
     configureWebpack: () => ({
+        resolve: {
+            alias: {
+                '@components': path.resolve(__dirname, 'src/components'),
+                '@store': path.resolve(__dirname, 'src/store'),
+                '@assets': path.resolve(__dirname, 'src/assets'),
+                '@router': path.resolve(__dirname, 'src/router'),
+            }
+        },
+
         devServer: {
             host: 'localhost',
             port: 8080,
